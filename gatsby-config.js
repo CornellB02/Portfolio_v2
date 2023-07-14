3ecde2ff96fb6ff99a86adf262a77c1b684de57d
Cornell Bethea <betheacornellb@gmail.com>
2023-07-14 19:12:48 -0400

    deplo

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
     title: 'Cornell Bethea Jr',
     description:
       "A Software Engineer who combines my love for technology with a relatable and approachable demeanor. I'm all about creating powerful software solutions while fostering a friendly and inclusive atmosphere. Let's connect!",
-    siteUrl: 'https://github.com/CornellB02/Portfolio_v2/blob/main/static', // No trailing slash allowed!
-    image: '/ogg.jpg', // Path to your image you placed in the 'static' folder
+    siteUrl: 'https://ibb.co', // No trailing slash allowed!
+    image: '/PwCvbrN', // Path to your image you placed in the 'static' folder
     twitterUsername: '@cbethea02',
   },
   plugins: [
