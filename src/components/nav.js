6acdd1dd848ee2f7ab174be49389f7243fc34822
Cornell Bethea <betheacornellb@gmail.com>
2023-07-20 13:22:19 -0400

    resumeupdate

--- src/components/nav.js
+++ src/components/nav.js
@@ -206,7 +206,7 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="https://docs.google.com/document/d/1D1gwJ0hFrNvJm7_qA3VP6KVEh_HQfKgsynRdMKWUDZc/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
